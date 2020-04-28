3406c6af7c61b2a8095d0914fa6fb589fcb97e01
Ryan Wilson <rywils21@gmail.com>
2020-04-28 09:19:15 -0500

    Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,35 +14,22 @@ export default () => {
       </Head>
 
       <div className="relative overflow-hidden">
-        <div className="max-w-screen-xl mx-auto ">
-          <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div className="border border-gray-200 bg-white rounded p-6 flex flex-col justify-between leading-normal shadow">
-                <div className="sm:text-center lg:text-left">
-                  <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                    Learn fullstack web development
-                  </h2>
-                  <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    Web Development Foundations is a free online course designed
-                    to help you build modern web applications.
-                  </p>
-                  <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                    <div className="inline-flex rounded-md shadow">
-                      <Link href="/course/software-engineer-crash-course/ui-building-blocks">
-                        <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-                          Get started
-                        </a>
-                      </Link>
-                    </div>
-                    <div className="ml-3 inline-flex">
-                      <Link href="/course/software-engineer-crash-course">
-                        <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out">
-                          Learn more
-                        </a>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
+        <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+          <div className="text-center">
+            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Program with <span className="text-indigo-600">Purpose</span>
+            </h2>
+            <p className="mt-3 max-w-md sm:max-w-xl mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-2xl">
+              Build real software by following courses, tutorials, and articles
+              written by someone building real software.
+            </p>
+            <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+              <div className="rounded-md shadow">
+                <Link href="/course/software-engineer-crash-course">
+                  <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                    Start Here
+                  </a>
+                </Link>
               </div>
             </div>
           </div>
